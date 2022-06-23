8d11aa2b577a817851056bd80e2fdbd10481c498
baselm <basel.magableh@icloud.com>
2022-06-23 10:34:56 +0100

    add progress bar

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,4 +1,4 @@
-import { Button, Container, Grid, Typography } from '@mui/material'
+import {  Container, Grid, LinearProgress, Typography } from '@mui/material'
 import React, { useEffect, useState } from 'react'
 import ProductCard from './ProductCard';
 
@@ -8,8 +8,6 @@ export default function Products() {
 
 // use State to monitor the products data
 const [products, setProducts] = useState([]);
-
-
 // use effect to get the data from the server api  
 useEffect( () => {
     fetch('http://localhost:8000/products')
@@ -19,27 +17,46 @@ useEffect( () => {
 }
     
 )
+// set a state for prgress bar
+const [progress, setProgress] =useState(0);
+
+useEffect(() => {
+    const timer = setInterval(() => {
+      setProgress((oldProgress) => {
+        if (oldProgress === 100) {
+          return 0;
+        }
+        const diff = Math.random() * 10;
+        return Math.min(oldProgress + diff, 100);
+      });
+    }, 500);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+  
 
   return (
     <Container>
-        <Grid container spacing={2}>
-            <Grid item xs={12} >
-                <Typography component={'h1'} variant='h1' color='Secondary' align='center'>
-                    Products Page
-                </Typography>
-                 <Typography>
-                    
-                 </Typography>
+        <Grid container >
+        <Grid item lg={12}>
+        <Typography component="h2" variant="h1" color="secondary" align="center" sx={{mt:4}}>
+                 Products Page 
+                 
+        </Typography>
+        <LinearProgress variant="determinate" value={progress} color="secondary"/>
 
-              
-            </Grid>
         </Grid>
-        <Grid container>
+        
+       
+        </Grid>
+        <Grid container pacing={2}>
                     {
                         products.map(product => (
 
 
-                            <Grid item xs={12} md={6} lg={3} key={product.id}>
+                            <Grid item xs={12} md={6} lg={4} key={product.id}>
                                 <ProductCard product={product} />
                                 </Grid>
                         ))
