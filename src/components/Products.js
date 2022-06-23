48fd3063959a2b463a303f81a91c2ebaa7c533f4
baselm <basel.magableh@icloud.com>
2022-06-23 09:38:27 +0100

    Add Routes and empty products

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,7 +1,16 @@
+import { Container, Grid, Typography } from '@mui/material'
 import React from 'react'
 
 export default function Products() {
   return (
-    <div>Products</div>
+    <Container>
+        <Grid container spacing={2}>
+            <Grid item xs={12} >
+                <Typography component={'h1'} variant='h1' color='Secondary' align='center'>
+                    Products Page
+                </Typography>
+            </Grid>
+        </Grid>
+    </Container>
   )
 }
