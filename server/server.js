12d2f0b7e8ac2885400e05a733ba7fd65468230e
baselm <basel.magableh@icloud.com>
2022-07-14 15:57:38 +0100

    up

--- server/server.js
+++ server/server.js
@@ -60,6 +60,10 @@ res.send({
 });
 });
 
+app.post('/api/users/login', (req, res) =>
+{
+  console.log('/api/users/login'); 
+  
+}); 
 
-
-app.listen(8080, () => console.log('API is running on http://localhost:8080/login'));
+app.listen(8080, () => console.log('API is running on http://localhost:8080/api/users/login'));
